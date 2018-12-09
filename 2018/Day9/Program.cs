88557b3abd4d03e7bbe4caf876c10c05646e70f9
mjsmith11 <mjsmith92@outlook.com>
2018-12-09 01:50:24 -0500

    Create Program.cs

---
+++ 2018/Day9/Program.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static int players = 435;
+        public static int lastMarble = 71184 * 100;
+        static void Main(string[] args)
+        {
+            List<int> scores = new List<int>();
+            for (int i = 0; i <= players; i++)
+            {
+                scores.Add(0);
+            }
+            List<int> marbles = new List<int>();
+            marbles.Add(0);
+            int nextMarble = 1;
+            int turn = 1;
+            int current = 0;
+            while (nextMarble <= lastMarble)
+            {
+                if(nextMarble % 1000 == 0)
+                {
+                    Console.WriteLine(nextMarble);
+                }
+                if (nextMarble % 23 == 0)
+                {
+                    scores[turn] += nextMarble;
+                    int take = current;
+                    for (int i = 0; i < 7; i++)
+                    {
+                        take = moveCounterClockwise(marbles, take);
+                    }
+                    scores[turn] += marbles[take];
+                    marbles.RemoveAt(take);
+                    current = take;
+                    if (current == marbles.Count())
+                    {
+                        current = 0;
+                    }
+                }
+                else
+                {
+                    int ind = moveClockwise(marbles, current);
+                    marbles.Insert(ind + 1, nextMarble);
+                    current = ind + 1;
+                }
+
+                nextMarble++;
+                turn = nextTurn(turn);
+
+            }
+            Console.WriteLine(scores.Max());
+            Console.ReadLine();
+        }
+
+        public static int moveClockwise(List<int> list, int current)
+        {
+            current++;
+            if (current >= list.Count())
+            {
+                current = 0;
+            }
+            return current;
+        }
+
+        public static int moveCounterClockwise(List<int> list, int current)
+        {
+            current--;
+            if(current < 0)
+            {
+                current = list.Count() - 1;
+            }
+            return current;
+        }
+        public static int nextTurn(int turn)
+        {
+            turn++;
+            if (turn > players)
+            {
+                turn = 1;
+            }
+            return turn;
+        }
+    }
+}
