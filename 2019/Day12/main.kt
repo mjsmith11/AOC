352507f245e62c1d506da5db5c755a4c5beb0246
Matt Smith <mjsmith92@outlook.com>
2019-12-12 19:36:25 -0500

    Solve Day 12 Part 2

--- 2019/Day12/main.kt
+++ 2019/Day12/main.kt
@@ -23,6 +23,16 @@ class Moon(var px: Int, var py: Int, var pz: Int, var vx: Int, var vy: Int, var 
     fun getEnergy(): Int {
         return (abs(px) + abs(py) + abs(pz)) * (abs(vx) + abs(vy) + abs(vz))
     }
+    fun xEqual(other: Moon): kotlin.Boolean {
+        return ((other.px == px) && (other.vx == vx))
+    }
+    fun yEqual(other: Moon): kotlin.Boolean {
+        return ((other.py == py) && (other.vy == vy))
+    }
+    fun zEqual(other: Moon): kotlin.Boolean {
+        return ((other.pz == pz) && (other.vz == vz))
+    }
+
 }
 
 fun main(args: Array<String>) {
@@ -30,8 +40,15 @@ fun main(args: Array<String>) {
     var m2 = Moon(-1,7,13,0,0,0)
     var m3 = Moon(-19,12,5,0,0,0)
     var m4 = Moon(-6,-6,-4,0,0,0)
-
-    for (i in 1..1000) {
+    var m10 = Moon(-17,9,-5,0,0,0)
+    var m20 = Moon(-1,7,13,0,0,0)
+    var m30 = Moon(-19,12,5,0,0,0)
+    var m40 = Moon(-6,-6,-4,0,0,0)
+    var xPeriod = 0
+    var yPeriod = 0
+    var zPeriod = 0
+    var counter = 0
+    while (xPeriod==0 || yPeriod==0 || zPeriod == 0) {
         m1.applyGravity(m2)
         m1.applyGravity(m3)
         m1.applyGravity(m4)
@@ -52,9 +69,30 @@ fun main(args: Array<String>) {
         m2.move()
         m3.move()
         m4.move()
+        
+        // increment the counter and check for stuff
+        counter = counter + 1
+        if (counter==1000) {
+            val energy = m1.getEnergy() + m2.getEnergy() + m3.getEnergy() + m4.getEnergy()
+            println("Part 1 : $energy")
+        }
+        // part 2 strategy it is periodic on a smaller period for each axis, find the lcm of these
+        if (xPeriod == 0) {
+            if (m1.xEqual(m10) && m2.xEqual(m20) && m3.xEqual(m30) && m4.xEqual(m40)) {
+                xPeriod = counter
+            }
+        }
+        if (yPeriod == 0) {
+            if (m1.yEqual(m10) && m2.yEqual(m20) && m3.yEqual(m30) && m4.yEqual(m40)) {
+                yPeriod = counter
+            }
+        }
+        if (zPeriod == 0) {
+            if (m1.zEqual(m10) && m2.zEqual(m20) && m3.zEqual(m30) && m4.zEqual(m40)) {
+                zPeriod = counter
+            }
+        }   
     }
-
-    val energy = m1.getEnergy() + m2.getEnergy() + m3.getEnergy() + m4.getEnergy()
-    println("$energy")
-
+    // use something external to compute the lcm like wolframalpha.com
+    println("Part 2 : lcm($xPeriod,$yPeriod,$zPeriod)")
 }
