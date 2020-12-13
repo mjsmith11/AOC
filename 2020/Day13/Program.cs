5217d98febf6a6102921eecbd406caba482e99ec
Matt Smith <mjsmith92@outlook.com>
2020-12-13 13:03:26 -0500

    2020: Use a much more efficient approach to part 2

--- 2020/Day13/Program.cs
+++ 2020/Day13/Program.cs
@@ -15,7 +15,6 @@ namespace Day13
             string[] input = getInput();
             int arrival = int.Parse(input[0]);
             string[] buses = input[1].Split(',');
-            //List<int> runningBuses = new List<int>();
             int minWait = int.MaxValue;
             int minWaitBus = 0;
             foreach(string bus in buses) {
@@ -38,34 +37,45 @@ namespace Day13
             return minWaitBus * minWait;
         }
 
+
+        // the idea here is that the possible timestamps for any group of buses
+        // can be given by t=mx+b where x is 0,1,2,3.  Start with the first bus: m will be the
+        // bus number and b will be 0.  Then add in the rest of the buses one at a time by
+        // finding the first two times given by the current values of m and b that satisfy the 
+        // new bus.  Then calculate new m and b using the ordered pairs (0,firstFoundTime) and (1, secondFoundTime).
+        // After all buses are accounted for, b will be the lowest time that satisfies all buses (i.e. x=0).
         static long part2() {
             string input = getInput()[1];
             string[] buses = input.Split(',');
-            int firstBus = int.Parse(buses[0]);
-            long time = 601 + 37; //this is a hack because I know my largest bus number is 601 at t+37
-            bool foundSolution = false;
-            long timecheck = 1;
-            while(!foundSolution) {
-                if (time > timecheck) {
-                    Console.WriteLine("Time exceeded " + timecheck);
-                    timecheck *= 10;
-                }
-                //assume this time will work
-                foundSolution = true;
-                for(int i = 0; i<buses.Length; i++) {
-                    if(buses[i] == "x") { continue; }
-                    if ((time + i) % int.Parse(buses[i]) != 0) {
-                        foundSolution = false;
-                        break;
+            //init stuff for the first bus
+            long m = int.Parse(buses[0]);
+            long b = 0;
+            
+            // figure in the rest of the buses
+            for(int i=1; i<buses.Length; i++) {
+                if (buses[i] == "x") { continue; }
+                long first = 0;
+                long second = 0;
+                int x = 0;
+                int busNum = int.Parse(buses[i]);
+                while (second == 0) {
+                    long time = m*x + b;
+                    if ((time + i) % busNum == 0) {
+                        if (first == 0) {
+                            first = time;
+                        } else {
+                            second = time;
+                        }
                     }
+                    x++;
                 }
-                if (!foundSolution) {
-                    time += 601; //this is a hack because I know my largest bus number is 601 at t+37
-                }
+                m = second - first;
+                b = first;
             }
-            return time;
+            return b;
         }
 
+ 
         static string[] getInput() {
             string inp = @"1002462
 37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,601,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,17,x,x,x,x,x,23,x,x,x,x,x,29,x,443,x,x,x,x,x,x,x,x,x,x,x,x,13";
