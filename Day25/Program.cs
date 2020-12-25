107d1c68d605a006bea4c0c61ef5f7f51cb3ff22
Matt Smith <mjsmith92@outlook.com>
2020-12-25 08:31:48 -0500

    2020: Solve day 25

---
+++ Day25/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Day25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Part 1: " + part1());
+        }
+        static long part1() {
+            // try to find the loop size
+            long target = 5290733; // first input
+            long value = 1;
+            long subject = 7;
+            int loops = 0;
+            while(value != target) {
+                value = value * subject;
+                value = value % 20201227;
+                loops++;
+            }
+
+            // transform the other key
+            subject = 15231938; // input 2
+            value = 1;
+            for(int i=0; i< loops; i++) {
+                value = value * subject;
+                value = value % 20201227;
+            }
+            return value;
+        }
+    }
+}
