40589523e5e62c192c6adf61d59f2ab0e9b638ba
Matt Smith <mjsmith92@outlook.com>
2022-12-15 17:31:40 -0500

    2022: Rewrite Day 15 part 2 to make it fast enough but still slow

--- 2022/Day15/main.cc
+++ 2022/Day15/main.cc
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <vector>
 #include <unordered_set>
+// #include <pair>
 
 #include "absl/strings/str_split.h"
 
@@ -50,12 +51,96 @@ int Part1(int targetRow) {
     }
     return nobeacons.size()-targetrowbecons.size();
 }
-
-long Part2(int maxCoord) {
+std::ostream&
+operator<<( std::ostream& dest, __int128_t value )
+{
+    std::ostream::sentry s( dest );
+    if ( s ) {
+        __uint128_t tmp = value < 0 ? -value : value;
+        char buffer[ 128 ];
+        char* d = std::end( buffer );
+        do
+        {
+            -- d;
+            *d = "0123456789"[ tmp % 10 ];
+            tmp /= 10;
+        } while ( tmp != 0 );
+        if ( value < 0 ) {
+            -- d;
+            *d = '-';
+        }
+        int len = std::end( buffer ) - d;
+        if ( dest.rdbuf()->sputn( d, len ) != len ) {
+            dest.setstate( std::ios_base::badbit );
+        }
+    }
+    return dest;
+}
+__int128_t Part2(int maxCoord) {
     vector<string> strs = getInput();
-    for(int r=0; r<=maxCoord; r++) {
-        unordered_set<int> nobeacons;
-        unordered_set<int> targetrowbecons;
+    unordered_set<__int128_t> possibilities;
+    for(int i=0; i<strs.size(); i++) {
+        if (strcmp("",strs[i].c_str()) == 0) { continue; }
+        vector<string> nums = absl::StrSplit(strs[i],' ');
+        int sx = atoi(nums[0].c_str());
+        int sy = atoi(nums[1].c_str());
+        int bx = atoi(nums[2].c_str());
+        int by = atoi(nums[3].c_str());
+
+        int dist = abs(sx-bx) + abs(sy-by);
+        int top = sy-dist;
+        int bottom = sy+dist;
+        int left = sx-dist;
+        int right = sx+dist;
+
+        // top to right
+        int cx = sx;
+        int cy = top-1;
+        while(cx<=right) {
+            if(cx>=0 && cx<=maxCoord && cy>=0 && cy<=maxCoord) {
+                possibilities.insert(4000000*static_cast<__int128_t>(cx)+cy);
+            }
+            cx++;
+            cy++;
+        }
+        // right to bottom
+        cx = right+1;
+        cy = sy;
+        while(cy<=bottom) {
+            if(cx>=0 && cx<=maxCoord && cy>=0 && cy<=maxCoord) {
+                possibilities.insert(4000000*static_cast<__int128_t>(cx)+cy);
+            }
+            cx--;
+            cy++;
+        }
+
+        // bottom to left
+        cx = sx;
+        cy = bottom+1;
+        while(cx>=left) {
+            if(cx>=0 && cx<=maxCoord && cy>=0 && cy<=maxCoord) {
+                possibilities.insert(4000000*static_cast<__int128_t>(cx)+cy);
+            }
+            cx--;
+            cy--;
+        }
+
+        // left to top
+        cx = left-1;
+        cy = sy;
+        while(cy>=top) {
+            if(cx>=0 && cx<=maxCoord && cy>=0 && cy<=maxCoord) {
+                possibilities.insert(4000000*static_cast<__int128_t>(cx)+cy);
+            }
+            cx++;
+            cy--;
+        }
+
+    }
+    for(__int128_t n: possibilities) {
+        int cx = n/4000000;
+        int cy = n%4000000;
+        bool good = true;
         for(int i=0; i<strs.size(); i++) {
             if (strcmp("",strs[i].c_str()) == 0) { continue; }
             vector<string> nums = absl::StrSplit(strs[i],' ');
@@ -63,40 +148,22 @@ long Part2(int maxCoord) {
             int sy = atoi(nums[1].c_str());
             int bx = atoi(nums[2].c_str());
             int by = atoi(nums[3].c_str());
+            if (cx==bx && cy==by) {
+                good = false;
+                break;
+            }
+            int dist = abs(sx-bx)+abs(sy-by);
+            int cdist = abs(cx-sx) + abs(cy-sy);
+            if(cdist<=dist) {
+                good = false;
+                break;
+            }
+        }
+        if (good) { return n; }
 
-            if (by==r) {
-                targetrowbecons.insert(bx);
-            }
-
-            int dist = abs(sx-bx) + abs(sy-by);
-            int yPart = abs(r-sy);
-            int maxX = dist-yPart;
-            if(maxX > 0) {
-                int start = sx-maxX>0? sx-maxX : 0;
-                int end = sx+maxX<maxCoord? sx+maxX : maxCoord;
-                for(int i=start; i<= end; i++) {
-                    nobeacons.insert(i);
-                }
-            }
-        }
-        if (r%100 == 0) {
-            cout << "Row " << r << " Impossible locations: " << nobeacons.size()-targetrowbecons.size() << " Beacons " << targetrowbecons.size() << "\n";
-        }
-        if (nobeacons.size() + targetrowbecons.size() < maxCoord+1) {
-            // find x
-            __int128_t total = 0;
-            for(int n : nobeacons) {
-                total += static_cast<__int128_t>(n);
-            }
-            for(int n : targetrowbecons) {
-                total += static_cast<__int128_t>(n);
-            }
-            int x = maxCoord*(maxCoord+1)/2 - total;
-            cout << "  ("<< x <<", "<< r <<")\n";
-            return x*4000000+r;
-        }
     }
     return 2;
+
 }
 // Driver Code
 int main()
