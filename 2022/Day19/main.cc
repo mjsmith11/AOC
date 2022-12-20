81716701d0f5905502dd5a874173f5c9c6f3126b
Matt Smith <mjsmith92@outlook.com>
2022-12-19 19:51:31 -0500

    More broken 2022:19 code

--- 2022/Day19/main.cc
+++ 2022/Day19/main.cc
@@ -12,7 +12,7 @@
 using namespace std;
 
 vector<string> getInput() {
-    ifstream myfile ("Day18/input.txt");
+    ifstream myfile ("Day19/input.txt");
     vector<string> inp;
     while(myfile) {
         string s;
@@ -67,24 +67,16 @@ State mine(State s, int rounds) {
 }
 map<string, int> cache;
 
-int maxGeodes2(State& s) {
-    // cout << "Caclulationg with " << s.minutes << "minutes\n";
+int maxGeodes2(State& s, int score) {
     if (s.minutes>24) {return 0; }
     if (s.minutes <= 0) {
         if (s.minutes < 0) {
             cout << "Minutes ERROR\n";
         }
-        return 0;
+        return score;
     }
-    // cout << "Before cache check\n";
-    if (auto r = cache.find(s.getString()); r != cache.end()) {
-        cout << "CACHE HIT!!\n";
-        return r->second;
-    }
-
-    int max = 0;
     int geodes = 0;
-
+    int max = s.geodes + s.geodeRobots*s.minutes;
     // possible actions
     // build a geodeRobot right now
     if (s.obsidian >= s.geodeObsidianCost && s.ore >= s.geodeOreCost) {
@@ -94,7 +86,7 @@ int maxGeodes2(State& s) {
         s1.obsidian -= s.geodeObsidianCost;
         s1.ore -= s.geodeOreCost;
 
-        int val = maxGeodes2(s1);
+        int val = maxGeodes2(s1, score+s1.minutes);
         if(val>max) {
             max = val;
             geodes = s1.geodes;
@@ -113,7 +105,7 @@ int maxGeodes2(State& s) {
                 s1.obsidian -= s.geodeObsidianCost;
                 s1.ore -= s.geodeOreCost;
 
-                int val = maxGeodes2(s1);
+                int val = maxGeodes2(s1, score+s1.minutes);
                 if(val>max) {
                     max = val;
                     geodes = s1.geodes;
@@ -122,110 +114,230 @@ int maxGeodes2(State& s) {
         }
     }
 
-    // build a obsidian right now
-    if (s.clay >= s.obsidianClayCost && s.ore >= s.obsidianOreCost) {
-        State s1 = mine(s, 1);
-        s1.minutes--;
-        s1.obsidianRobots++;
-        s1.clay -= s.obsidianClayCost;
-        s1.ore -= s.obsidianOreCost;
+    if (s.obsidianRobots < s.geodeObsidianCost) {
+        // build a obsidian right now
+        if (s.clay >= s.obsidianClayCost && s.ore >= s.obsidianOreCost) {
+            State s1 = mine(s, 1);
+            s1.minutes--;
+            s1.obsidianRobots++;
+            s1.clay -= s.obsidianClayCost;
+            s1.ore -= s.obsidianOreCost;
 
-        int val = maxGeodes2(s1);
-        if(val>max) {
-            max = val;
-            geodes = s1.geodes;
-        }
-    } else { // wait until we can build a obsidian
-        if (s.clayRobots>0) { // if we aren't mining clay, we can't build it
-            int extraClayNeeded = s.obsidianClayCost - s.clay;
-            int extraOreNeeded = s.obsidianOreCost - s.ore;
-            int clayRounds = ceil(static_cast<double>(extraClayNeeded)/static_cast<double>(s.clayRobots));
-            int oreRounds = ceil(static_cast<double>(extraOreNeeded)/static_cast<double>(s.oreRobots));
-            int rounds = clayRounds>oreRounds?clayRounds:oreRounds+1; // add 1 for this round
-            if (rounds <= s.minutes) {
-                State s1 = mine(s, rounds);
-                s1.minutes -= rounds;
-                s1.obsidianRobots++;
-                s1.clay -= s.obsidianClayCost;
-                s1.ore -= s.obsidianOreCost;
+            int val = maxGeodes2(s1, score);
+            if(val>max) {
+                max = val;
+                geodes = s1.geodes;
+            }
+        } else { // wait until we can build a obsidian
+            if (s.clayRobots>0) { // if we aren't mining clay, we can't build it
+                int extraClayNeeded = s.obsidianClayCost - s.clay;
+                int extraOreNeeded = s.obsidianOreCost - s.ore;
+                int clayRounds = ceil(static_cast<double>(extraClayNeeded)/static_cast<double>(s.clayRobots));
+                int oreRounds = ceil(static_cast<double>(extraOreNeeded)/static_cast<double>(s.oreRobots));
+                int rounds = clayRounds>oreRounds?clayRounds:oreRounds+1; // add 1 for this round
+                if (rounds <= s.minutes) {
+                    State s1 = mine(s, rounds);
+                    s1.minutes -= rounds;
+                    s1.obsidianRobots++;
+                    s1.clay -= s.obsidianClayCost;
+                    s1.ore -= s.obsidianOreCost;
 
-                int val = maxGeodes2(s1);
-                if(val>max) {
-                    max = val;
-                    geodes = s1.geodes;
+                    int val = maxGeodes2(s1, score);
+                    if(val>max) {
+                        max = val;
+                        geodes = s1.geodes;
+                    }
                 }
             }
+
         }
     }
 
-    // build a clay robot right now
-    if (s.ore >= s.clayCost) {
-        State s1 = mine(s, 1);
-        s1.minutes--;
-        s1.clayRobots++;
-        s1.ore -= s.clayCost;
-
-        int val = maxGeodes2(s1);
-        if(val>max) {
-            max = val;
-            geodes = s1.geodes;
-        }
-    } else { // wait until we can build a clay
-        int extraOreNeeded = s.clayCost - s.ore;
-        int rounds = ceil(static_cast<double>(extraOreNeeded)/static_cast<double>(s.oreRobots))+1;
-        if (rounds <= s.minutes) {
-            State s1 = mine(s, rounds);
-            s1.minutes -= rounds;
+    if (s.clayRobots < s.obsidianClayCost) {
+        // build a clay robot right now
+        if (s.ore >= s.clayCost) {
+            State s1 = mine(s, 1);
+            s1.minutes--;
             s1.clayRobots++;
             s1.ore -= s.clayCost;
 
-            int val = maxGeodes2(s1);
+            int val = maxGeodes2(s1, score);
             if(val>max) {
                 max = val;
                 geodes = s1.geodes;
             }
+        } else { // wait until we can build a clay
+            int extraOreNeeded = s.clayCost - s.ore;
+            int rounds = ceil(static_cast<double>(extraOreNeeded)/static_cast<double>(s.oreRobots))+1;
+            if (rounds <= s.minutes) {
+                State s1 = mine(s, rounds);
+                s1.minutes -= rounds;
+                s1.clayRobots++;
+                s1.ore -= s.clayCost;
+
+                int val = maxGeodes2(s1, score);
+                if(val>max) {
+                    max = val;
+                    geodes = s1.geodes;
+                }
+            }
         }
     }
 
-    // build a ore robot right now
-    if (s.ore >= s.oreCost) {
-        State s1 = mine(s, 1);
-        s1.minutes--;
-        s1.oreRobots++;
-        s1.ore -= s.oreCost;
+    int maxOreNeeded = s.oreCost>s.clayCost?s.oreCost:s.clayCost;
+    maxOreNeeded = maxOreNeeded>s.obsidianOreCost?maxOreNeeded:s.obsidianOreCost;
+    maxOreNeeded = maxOreNeeded>s.geodeOreCost?maxOreNeeded:s.geodeOreCost;
 
-        int val = maxGeodes2(s1);
-        if(val>max) {
-            max = val;
-            geodes = s1.geodes;
-        }
-    } else { // wait until we can build a clay
-        int extraOreNeeded = s.oreCost - s.ore;
-        int rounds = ceil(static_cast<double>(extraOreNeeded)/static_cast<double>(s.oreRobots))+1;
-        if (rounds<=s.minutes) {
-            State s1 = mine(s, rounds);
-            s1.minutes -= rounds;
+    if (s.oreRobots < maxOreNeeded) {
+        // build a ore robot right now
+        if (s.ore >= s.oreCost) {
+            State s1 = mine(s, 1);
+            s1.minutes--;
             s1.oreRobots++;
             s1.ore -= s.oreCost;
 
-            int val = maxGeodes2(s1);
+            int val = maxGeodes2(s1, score);
             if(val>max) {
                 max = val;
                 geodes = s1.geodes;
             }
+        } else { // wait until we can build a ore
+            int extraOreNeeded = s.oreCost - s.ore;
+            int rounds = ceil(static_cast<double>(extraOreNeeded)/static_cast<double>(s.oreRobots))+1;
+            if (rounds<=s.minutes) {
+                State s1 = mine(s, rounds);
+                s1.minutes -= rounds;
+                s1.oreRobots++;
+                s1.ore -= s.oreCost;
+
+                int val = maxGeodes2(s1, score);
+                if(val>max) {
+                    max = val;
+                    geodes = s1.geodes;
+                }
+            }
         }
     }
-    // wait until the end
-    int val = s.geodeRobots*s.minutes;
+    
+    // nothing
+    /*int val = 0;
     if (val > max) {
+        cout << "Setting max to 0\n";
         geodes = 0;
         max = val;
-    }
+    }*/
     return max;
-    return geodes+max-s.geodeRobots;
 }
 
-int maxGeodes(State& s) {
+int maxGeodes3(State& s) {
+    if (s.minutes>24) {return 0; }
+    if (s.minutes <= 0) {
+        if (s.minutes < 0) {
+            cout << "Minutes ERROR\n";
+        }
+        return s.geodes;
+    }
+    int geodes = 0;
+    int max = s.geodes + s.geodeRobots*s.minutes;
+    // possible actions
+    // build a geodeRobot right now
+    if (s.obsidianRobots>0 && s.oreRobots>0) {
+        int wait = 0;
+        int obAmt = ceil(s.geodeObsidianCost-s.obsidian/static_cast<double>(s.obsidianRobots));
+        int orrAmt = ceil(s.geodeOreCost-s.ore/static_cast<double>(s.oreRobots));
+        wait = std::max(wait, obAmt);
+        wait = std::max(wait, orrAmt);
+        int remtime = s.minutes - wait - 1;
+        if (remtime>0) {
+            State s1 = s;
+            s1.minutes = remtime;
+
+            s1.geodes = s.geodes+s.geodeRobots*(wait+1);
+            s1.obsidian = s.obsidian+s.obsidianRobots*(wait+1);
+            s1.geodes = s.clay+s.clayRobots*(wait+1);
+            s1.geodes = s.ore+s.oreRobots*(wait+1);
+            s1.obsidian -= s.geodeObsidianCost;
+            s1.ore -= s.geodeOreCost;
+            s1.geodeRobots++;
+            max = std::max(maxGeodes3(s1), max);
+        }
+    }
+    
+    if (s.clayRobots>0 && s.oreRobots>0 && s.obsidianRobots < s.geodeObsidianCost) {
+        int wait = 0;
+        int clayAmt = ceil(s.obsidianClayCost-s.clay/static_cast<double>(s.clayRobots));
+        int orrAmt = ceil(s.obsidianOreCost-s.ore/static_cast<double>(s.oreRobots));
+        wait = std::max(wait, clayAmt);
+        wait = std::max(wait, orrAmt);
+        int remtime = s.minutes - wait - 1;
+        if (remtime>0) {
+            State s1 = s;
+            s1.minutes = remtime;
+
+            s1.geodes = s.geodes+s.geodeRobots*(wait+1);
+            s1.obsidian = s.obsidian+s.obsidianRobots*(wait+1);
+            s1.geodes = s.clay+s.clayRobots*(wait+1);
+            s1.geodes = s.ore+s.oreRobots*(wait+1);
+            s1.clay -= s.obsidianClayCost;
+            s1.ore -= s.obsidianOreCost;
+            s1.obsidianRobots++;
+            max = std::max(maxGeodes3(s1), max);
+        }
+    }
+
+    if (s.oreRobots>0 && s.clayRobots < s.obsidianClayCost) {
+        int wait = 0;
+        int orrAmt = ceil(s.clayCost-s.ore/static_cast<double>(s.oreRobots));
+        wait = std::max(wait, orrAmt);
+        int remtime = s.minutes - wait - 1;
+        if (remtime>0) {
+            State s1 = s;
+            s1.minutes = remtime;
+
+            s1.geodes = s.geodes+s.geodeRobots*(wait+1);
+            s1.obsidian = s.obsidian+s.obsidianRobots*(wait+1);
+            s1.geodes = s.clay+s.clayRobots*(wait+1);
+            s1.geodes = s.ore+s.oreRobots*(wait+1);
+            s1.ore -= s.clayCost;
+            s1.clayRobots++;
+            max = std::max(maxGeodes3(s1), max);
+        }
+    }
+
+    int maxOreNeeded = s.oreCost>s.clayCost?s.oreCost:s.clayCost;
+    maxOreNeeded = maxOreNeeded>s.obsidianOreCost?maxOreNeeded:s.obsidianOreCost;
+    maxOreNeeded = maxOreNeeded>s.geodeOreCost?maxOreNeeded:s.geodeOreCost;
+
+    if (s.oreRobots>0 && s.oreRobots < maxOreNeeded) {
+        int wait = 0;
+        int orrAmt = ceil(s.oreCost-s.ore/static_cast<double>(s.oreRobots));
+        wait = std::max(wait, orrAmt);
+        int remtime = s.minutes - wait - 1;
+        if (remtime>0) {
+            State s1 = s;
+            s1.minutes = remtime;
+
+            s1.geodes = s.geodes+s.geodeRobots*(wait+1);
+            s1.obsidian = s.obsidian+s.obsidianRobots*(wait+1);
+            s1.geodes = s.clay+s.clayRobots*(wait+1);
+            s1.geodes = s.ore+s.oreRobots*(wait+1);
+            s1.ore -= s.oreCost;
+            s1.oreRobots++;
+            max = std::max(maxGeodes3(s1), max);
+        }
+    }
+    
+    // nothing
+    /*int val = 0;
+    if (val > max) {
+        cout << "Setting max to 0\n";
+        geodes = 0;
+        max = val;
+    }*/
+    return max;
+}
+
+int maxGeodes(State s) {
     // cout << "No vars" << s.ore << "\n";
     // cout << "Processing with " << s.minutes << " minutes and " << s.ore <<" ore\n";
     if (s.minutes>24) {return 0; }
@@ -383,7 +495,49 @@ int maxGeodes(State& s) {
 }
 
 int Part1() {
-    State start;
+    vector<string> inp = getInput();
+    int quality = 0;
+    for(string s : getInput()) {
+        if (strcmp("",s.c_str()) == 0) { continue; }
+        vector<string> words = absl::StrSplit(s,' ');
+        vector<string> blueprintWords = absl::StrSplit(words[1],':');
+        int blueprintNum = atoi(blueprintWords[0].c_str());
+        // cout << blueprintNum << "\n";
+        State start;
+        start.minutes = 24;
+
+        start.oreRobots = 1;
+        start.clayRobots = 0;
+        start.obsidianRobots = 0;
+        start.geodeRobots = 0;
+
+        start.oreCost = atoi(words[6].c_str());
+        // cout << start.oreCost << "\n";
+        start.clayCost = atoi(words[12].c_str());
+        // cout << start.clayCost << "\n";
+        start.obsidianOreCost = atoi(words[18].c_str());
+        // cout << start.obsidianOreCost << "\n";
+        start.obsidianClayCost = atoi(words[21].c_str());
+        // cout << start.obsidianClayCost << "\n";
+        start.geodeOreCost = atoi(words[27].c_str());
+        // cout << start.geodeOreCost << "\n";
+        start.geodeObsidianCost = atoi(words[30].c_str());
+        // cout << start.geodeObsidianCost<< "\n";
+
+        start.ore = 0;
+        start.clay = 0;
+        start.obsidian = 0;
+        start.geodes = 0;
+
+        cache.clear();
+        // cout << "Simulating blueprint # " << blueprintNum << "\n";
+        int n = maxGeodes3(start);
+        quality += blueprintNum * n;
+        cout << blueprintNum << " : " << n<< "\n";
+    }
+    return quality;
+
+    /*State start;
     start.minutes = 24;
 
     start.oreRobots = 1;
@@ -391,19 +545,19 @@ int Part1() {
     start.obsidianRobots = 0;
     start.geodeRobots = 0;
 
-    start.oreCost = 4;
-    start.clayCost = 2;
+    start.oreCost = 2;
+    start.clayCost = 3;
     start.obsidianOreCost = 3;
-    start.obsidianClayCost = 14;
-    start.geodeObsidianCost = 7;
-    start.geodeOreCost = 2;
+    start.obsidianClayCost = 8;
+    start.geodeObsidianCost = 12;
+    start.geodeOreCost = 3;
 
     start.ore = 0;
     start.clay = 0;
     start.obsidian = 0;
     start.geodes = 0;
     
-    return maxGeodes2(start);
+    return maxGeodes2(start,0);*/
 }
 
 int Part2() {
