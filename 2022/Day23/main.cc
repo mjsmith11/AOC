fb12d4b501d0218676caf991d7978b25e82df0a5
Matt Smith <mjsmith92@outlook.com>
2022-12-23 14:21:13 -0500

    Finish 2022 Day 23

--- 2022/Day23/main.cc
+++ 2022/Day23/main.cc
@@ -26,31 +26,12 @@ struct Elf {
     pair<int,int> proposedLocation;
     bool willMove;
 };
-/*struct pair_hash {
+struct pair_hash {
     inline std::size_t operator()(const std::pair<int,int> & v) const {
         return v.first*31+v.second;
     }
-};*/
-struct pair_hash
-{
-    template <class T1, class T2>
-    std::size_t operator() (const std::pair<T1, T2> &pair) const {
-        return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
-    }
 };
-void output(unordered_set<pair<int,int>, pair_hash> occupiedLocations) {
-    for(int y=-5; y<15; y++) {
-        for(int x=-5; x<15;x++) {
-            if (occupiedLocations.find(pair<int,int>(x,y)) == occupiedLocations.end()) {
-                cout << ".";
-            } else {
-                cout << "#";
-            }
-        }
-        cout << "\n";
-    }
-    cout << "\n";
-}
+
 int Part1() {
     vector<Elf> elves;
     unordered_set<pair<int,int>, pair_hash> occupiedLocations;
@@ -66,38 +47,33 @@ int Part1() {
             }
         }
     }
-    // output(occupiedLocations);
     map<pair<int,int>, int> numProposals;
     vector<char> order;
     order.push_back('N');
     order.push_back('S');
     order.push_back('W');
     order.push_back('E');
-    for(int i=0; i<2; i++) {
+    for(int i=0; i<10; i++) {
         // propose locations
         for (Elf& e : elves) {
-            pair<int, int> ch1(e.location.first-1, e.location.second-1);
-            pair<int, int> ch2(e.location.first, e.location.second-1);
-            pair<int, int> ch3(e.location.first+1, e.location.second-1);
-            pair<int, int> ch4(e.location.first-1, e.location.second);
-            pair<int, int> ch5(e.location.first+1, e.location.second);
-            pair<int, int> ch6(e.location.first-1, e.location.second+1);
-            pair<int, int> ch7(e.location.first, e.location.second+1);
-            pair<int, int> ch8(e.location.first+1, e.location.second+1);
-            bool clear = (occupiedLocations.find(ch1) == occupiedLocations.end());
-            clear &= (occupiedLocations.find(ch2) == occupiedLocations.end());
-            clear &= (occupiedLocations.find(ch3) == occupiedLocations.end());
-            clear &= (occupiedLocations.find(ch4) == occupiedLocations.end());
-            clear &= (occupiedLocations.find(ch5) == occupiedLocations.end());
-            clear &= (occupiedLocations.find(ch6) == occupiedLocations.end());
-            clear &= (occupiedLocations.find(ch7) == occupiedLocations.end());
-            clear &= (occupiedLocations.find(ch8) == occupiedLocations.end());
+            bool clear = true;
+            for(int x=-1; x<=1; x++) {
+                for(int y=-1; y<=1; y++) {
+                    if (x==0 && y==0) {
+                        continue;
+                    }
+                    if (occupiedLocations.find(pair<int,int>(e.location.first + x, e.location.second+y)) != occupiedLocations.end()) {
+                        clear = false;
+                    }
+                }
+            }
             if (clear) {
                 e.willMove = false;
                 continue;
             } else {
                 e.willMove = true;
             }
+            bool madeProposal = false;
             for(char o : order) {
                 pair<int,int> c1;
                 pair<int,int> c2;
@@ -126,6 +102,7 @@ int Part1() {
                     if (occupiedLocations.find(c2) == occupiedLocations.end()) {
                         if (occupiedLocations.find(c3) == occupiedLocations.end()) {
                             e.proposedLocation = c2;
+                            madeProposal = true;
                             if (numProposals.find(c2) == numProposals.end()) {
                                 numProposals[c2] = 1;
                             } else {
@@ -136,6 +113,9 @@ int Part1() {
                     }
                 }
             }
+            if (!madeProposal) {
+                e.willMove = false;
+            }
         }
         // move the elves
         occupiedLocations.clear();
@@ -145,7 +125,6 @@ int Part1() {
             }
             occupiedLocations.insert(e.location);
         }
-        output(occupiedLocations);
 
         //reset
         numProposals.clear();
@@ -154,6 +133,7 @@ int Part1() {
         newOrder.push_back(order[2]);
         newOrder.push_back(order[3]);
         newOrder.push_back(order[0]);
+        order = newOrder;
     }
     // calculate result
     int minX = elves[0].location.first;
@@ -166,15 +146,119 @@ int Part1() {
         maxX = maxX>e.location.first?maxX:e.location.first;
         maxY = maxY>e.location.second?maxY:e.location.second;
     }
-    cout << minX << "\n";
-    cout << maxX << "\n";
-    cout << minY << "\n";
-    cout << maxY << "\n";
+
     return (maxX-minX+1)*(maxY-minY+1)-elves.size();
 }
 
+
 int Part2() {
-    return 2;
+    vector<Elf> elves;
+    unordered_set<pair<int,int>, pair_hash> occupiedLocations;
+    vector<string> inp = getInput();
+    for(int y=0; y<inp.size(); y++) {
+        for(int x=0; x<inp[y].length(); x++) {
+            if(inp[y][x] == '#') {
+                Elf e;
+                e.location = pair<int,int>(x,y);
+                e.willMove = false;
+                elves.push_back(e);
+                occupiedLocations.insert(e.location);
+            }
+        }
+    }
+    map<pair<int,int>, int> numProposals;
+    vector<char> order;
+    order.push_back('N');
+    order.push_back('S');
+    order.push_back('W');
+    order.push_back('E');
+    for(int i=0; i<1000; i++) {
+        // propose locations
+        for (Elf& e : elves) {
+            bool clear = true;
+            for(int x=-1; x<=1; x++) {
+                for(int y=-1; y<=1; y++) {
+                    if (x==0 && y==0) {
+                        continue;
+                    }
+                    if (occupiedLocations.find(pair<int,int>(e.location.first + x, e.location.second+y)) != occupiedLocations.end()) {
+                        clear = false;
+                    }
+                }
+            }
+            if (clear) {
+                e.willMove = false;
+                continue;
+            } else {
+                e.willMove = true;
+            }
+            bool addedProposal = false;
+            for(char o : order) {
+                pair<int,int> c1;
+                pair<int,int> c2;
+                pair<int,int> c3;
+                if (o=='N') {
+                    c1 = pair<int,int>(e.location.first-1, e.location.second-1);
+                    c2 = pair<int,int>(e.location.first, e.location.second-1);
+                    c3 = pair<int,int>(e.location.first+1, e.location.second-1);
+                }
+                else if (o=='S') {
+                    c1 = pair<int,int>(e.location.first-1, e.location.second+1);
+                    c2 = pair<int,int>(e.location.first, e.location.second+1);
+                    c3 = pair<int,int>(e.location.first+1, e.location.second+1);
+                }
+                else if (o=='W') {
+                    c1 = pair<int,int>(e.location.first-1, e.location.second-1);
+                    c2 = pair<int,int>(e.location.first-1, e.location.second);
+                    c3 = pair<int,int>(e.location.first-1, e.location.second+1);
+                }
+                else if (o=='E') {
+                    c1 = pair<int,int>(e.location.first+1, e.location.second-1);
+                    c2 = pair<int,int>(e.location.first+1, e.location.second);
+                    c3 = pair<int,int>(e.location.first+1, e.location.second+1);
+                }
+                if (occupiedLocations.find(c1) == occupiedLocations.end()) {
+                    if (occupiedLocations.find(c2) == occupiedLocations.end()) {
+                        if (occupiedLocations.find(c3) == occupiedLocations.end()) {
+                            e.proposedLocation = c2;
+                            if (numProposals.find(c2) == numProposals.end()) {
+                                numProposals[c2] = 1;
+                            } else {
+                                numProposals[c2]++;
+                            }
+                            addedProposal  = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (!addedProposal) {e.willMove = false;}
+        }
+        // move the elves
+        occupiedLocations.clear();
+        bool anyMoved = false;
+        for (Elf& e : elves) {
+            if (e.willMove && numProposals[e.proposedLocation] == 1) {
+                anyMoved = true;
+                e.location = e.proposedLocation;
+            }
+            occupiedLocations.insert(e.location);
+        }
+        if (!anyMoved) {
+            return i+1;
+        }
+
+        //reset
+        numProposals.clear();
+        vector<char> newOrder;
+        newOrder.push_back(order[1]);
+        newOrder.push_back(order[2]);
+        newOrder.push_back(order[3]);
+        newOrder.push_back(order[0]);
+        order = newOrder;
+    }
+
+    return -1;
 }
 
 
