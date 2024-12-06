46b00e50954caafd671260b8cb9064a5c8b3ffb4
Matthew Smith <mjsmith92@outlook.com>
2024-12-06 15:06:09 -0500

    Messy day 6 but works

---
+++ 2024/Day06/main.go
@@ -0,0 +1,260 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sync"
+)
+
+func main() {
+	//fmt.Println("Part 1:", part1())
+	fmt.Println("Part 2:", part2())
+}
+
+func part1() int {
+	grid := readInput()
+	dir := 'U'
+	x, y := findGuard(grid)
+	grid[y][x] = 'X'
+	for {
+		if dir == 'U' {
+			if y == 0 {
+				// off grid
+				break
+			} else if grid[y-1][x] == '#' {
+				// obstacle
+				dir = 'R'
+			} else {
+				// walk
+				y--
+				grid[y][x] = 'X'
+			}
+		} else if dir == 'R' {
+			if x == len(grid[0])-1 {
+				// off grid
+				break
+			} else if grid[y][x+1] == '#' {
+				// obstacle
+				dir = 'D'
+			} else {
+				// walk
+				x++
+				grid[y][x] = 'X'
+			}
+		} else if dir == 'D' {
+			if y == len(grid)-1 {
+				// off grid
+				break
+			} else if grid[y+1][x] == '#' {
+				// obstacle
+				dir = 'L'
+			} else {
+				// walk
+				y++
+				grid[y][x] = 'X'
+			}
+		} else {
+			// L
+			if x == 0 {
+				// off grid
+				break
+			} else if grid[y][x-1] == '#' {
+				// obstacle
+				dir = 'U'
+			} else {
+				// walk
+				x--
+				grid[y][x] = 'X'
+			}
+		}
+	}
+	return countX(grid)
+}
+
+func part2() int {
+	//wg := &sync.WaitGroup{}
+	ans := 0
+	results := make(chan bool)
+	grid := readInput()
+	counter := 0
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid[0]); x++ {
+			fmt.Println(counter)
+			if grid[y][x] == '.' {
+				newGrid := make([][]rune, 0)
+				for iy := 0; iy < len(grid); iy++ {
+					line := make([]rune, 0)
+					for ix := 0; ix < len(grid[0]); ix++ {
+						if x == ix && y == iy {
+							line = append(line, '#')
+						} else {
+							line = append(line, grid[iy][ix])
+						}
+					}
+					newGrid = append(newGrid, line)
+				}
+				if hasCycle(newGrid, results) {
+					ans++
+				}
+				// wg.Add(1)
+				// go func() {
+				// 	defer wg.Done()
+				// 	hasCycle(newGrid, results)
+				// }()
+			}
+			counter++
+		}
+	}
+	//go monitor(wg, results)
+
+	// for v := range results {
+	// 	fmt.Println(v)
+	// }
+	return ans
+}
+
+func monitor(wg *sync.WaitGroup, ch chan bool) {
+	wg.Wait()
+	close(ch)
+}
+
+func stateToString(x, y int, dir rune) string {
+	return fmt.Sprintf("%v,%v,%v", x, y, dir)
+}
+func hasCycle(grid [][]rune, c chan bool) bool {
+	seen := make(map[string]bool, 0)
+	dir := 'U'
+	x, y := findGuard(grid)
+	seen[stateToString(x, y, dir)] = true
+	for {
+		//fmt.Println("loop")
+		if dir == 'U' {
+			if y == 0 {
+				// off grid
+				break
+			} else if grid[y-1][x] == '#' {
+				// obstacle
+				dir = 'R'
+			} else {
+				// walk
+				y--
+				state := stateToString(x, y, dir)
+				if _, ok := seen[state]; ok {
+					//c <- true
+					return true
+				} else {
+					seen[state] = true
+				}
+			}
+		} else if dir == 'R' {
+			if x == len(grid[0])-1 {
+				// off grid
+				break
+			} else if grid[y][x+1] == '#' {
+				// obstacle
+				dir = 'D'
+			} else {
+				// walk
+				x++
+				state := stateToString(x, y, dir)
+				if _, ok := seen[state]; ok {
+					//c <- true
+					return true
+				} else {
+					seen[state] = true
+				}
+			}
+		} else if dir == 'D' {
+			if y == len(grid)-1 {
+				// off grid
+				break
+			} else if grid[y+1][x] == '#' {
+				// obstacle
+				dir = 'L'
+			} else {
+				// walk
+				y++
+				state := stateToString(x, y, dir)
+				if _, ok := seen[state]; ok {
+					//c <- true
+					return true
+				} else {
+					seen[state] = true
+				}
+			}
+		} else {
+			// L
+			if x == 0 {
+				// off grid
+				break
+			} else if grid[y][x-1] == '#' {
+				// obstacle
+				dir = 'U'
+			} else {
+				// walk
+				x--
+				state := stateToString(x, y, dir)
+				if _, ok := seen[state]; ok {
+					//c <- true
+					return true
+				} else {
+					seen[state] = true
+				}
+			}
+		}
+	}
+	return false
+	//c <- false
+}
+
+func dumpGrid(grid [][]rune) {
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid[0]); x++ {
+			fmt.Print(string(grid[y][x]))
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
+func countX(grid [][]rune) int {
+	c := 0
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid[y]); x++ {
+			if grid[y][x] == 'X' {
+				c++
+			}
+		}
+	}
+	return c
+}
+
+func findGuard(grid [][]rune) (int, int) {
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid[y]); x++ {
+			if grid[y][x] == '^' {
+				return x, y
+			}
+		}
+	}
+	return -1, -1
+}
+
+func readInput() [][]rune {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	strs := make([][]rune, 0)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := make([]rune, 0)
+		for _, r := range scanner.Text() {
+			line = append(line, r)
+		}
+		strs = append(strs, line)
+	}
+	return strs
+}
